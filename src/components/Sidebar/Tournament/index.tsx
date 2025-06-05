7cc709434c124d21d2709ebc8f56f7b92042f9cb
SdazaP <creepersdp57@gmail.com>
2025-06-05 13:35:37 -0600

    feat(navbar): implement tournament navigation bar

---
+++ src/components/Sidebar/Tournament/index.tsx
@@ -0,0 +1,318 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import SidebarLinkGroup from './SidebarLinkGroup';
+import Logo from '../../../images/logo/logo.png';
+import ProfileImage from '../../../images/user/user-01.png'; // Asegúrate de tener esta imagen
+
+interface SidebarProps {
+  sidebarOpen: boolean;
+  setSidebarOpen: (arg: boolean) => void;
+}
+
+const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { pathname } = location;
+
+  const trigger = useRef<any>(null);
+  const sidebar = useRef<any>(null);
+
+  const storedSidebarExpanded = localStorage.getItem('sidebar-expanded');
+  const [sidebarExpanded, setSidebarExpanded] = useState(
+    storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true',
+  );
+
+  // close on click outside
+  useEffect(() => {
+    const clickHandler = ({ target }: MouseEvent) => {
+      if (!sidebar.current || !trigger.current) return;
+      if (
+        !sidebarOpen ||
+        sidebar.current.contains(target) ||
+        trigger.current.contains(target)
+      )
+        return;
+      setSidebarOpen(false);
+    };
+    document.addEventListener('click', clickHandler);
+    return () => document.removeEventListener('click', clickHandler);
+  });
+
+  // close if the esc key is pressed
+  useEffect(() => {
+    const keyHandler = ({ keyCode }: KeyboardEvent) => {
+      if (!sidebarOpen || keyCode !== 27) return;
+      setSidebarOpen(false);
+    };
+    document.addEventListener('keydown', keyHandler);
+    return () => document.removeEventListener('keydown', keyHandler);
+  });
+
+  useEffect(() => {
+    localStorage.setItem('sidebar-expanded', sidebarExpanded.toString());
+    if (sidebarExpanded) {
+      document.querySelector('body')?.classList.add('sidebar-expanded');
+    } else {
+      document.querySelector('body')?.classList.remove('sidebar-expanded');
+    }
+  }, [sidebarExpanded]);
+
+  const handleGoBack = () => {
+    navigate(-1); // Navega a la página anterior
+  };
+
+  return (
+    <aside
+      ref={sidebar}
+      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+        sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+      }`}
+    >
+      {/* <!-- SIDEBAR HEADER --> */}
+      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+        <NavLink to="/">
+          <img src={Logo} alt="Logo" />
+        </NavLink>
+
+        <button
+          ref={trigger}
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          aria-controls="sidebar"
+          aria-expanded={sidebarOpen}
+          className="block lg:hidden"
+        >
+          <svg
+            className="fill-current"
+            width="20"
+            height="18"
+            viewBox="0 0 20 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+              fill=""
+            />
+          </svg>
+        </button>
+      </div>
+      {/* <!-- SIDEBAR HEADER --> */}
+
+      {/* <!-- USER PROFILE IMAGE --> */}
+      <div className="flex flex-col items-center py-4">
+        <NavLink
+          to="/tournament"
+          className="rounded-full border-4 border-white p-1"
+        >
+          <img
+            src={ProfileImage}
+            alt="User Profile"
+            className="h-20 w-20 rounded-full object-cover"
+          />
+        </NavLink>
+        <span className="mt-2 text-sm font-medium text-white">
+          Nombre de Torneo
+        </span>
+      </div>
+
+      <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
+        {/* <!-- Sidebar Menu --> */}
+        <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
+          {/* <!-- Menu Group --> */}
+          <div>
+            <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+              MENU
+            </h3>
+
+            <ul className="mb-6 flex flex-col gap-1.5">
+              {/* <!-- Menu Item Participantes --> */}
+              <li>
+                <NavLink
+                  to="/participants"
+                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('participants') &&
+                    'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  <svg
+                    className="fill-current"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
+                      fill=""
+                    />
+                    <path
+                      d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
+                      fill=""
+                    />
+                  </svg>
+                  Participantes
+                </NavLink>
+              </li>
+
+              {/* <!-- Menu Item Resultados --> */}
+              <SidebarLinkGroup activeCondition={pathname.includes('results')}>
+                {(handleClick, open) => {
+                  return (
+                    <React.Fragment>
+                      <NavLink
+                        to="#"
+                        className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                          pathname.includes('results') &&
+                          'bg-graydark dark:bg-meta-4'
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded
+                            ? handleClick()
+                            : setSidebarExpanded(true);
+                        }}
+                      >
+                        <svg
+                          className="fill-current"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M16.8754 11.6719C16.5379 11.6719 16.2285 11.9531 16.2285 12.2906V15.0188C16.2285 15.4281 15.9035 15.7531 15.4942 15.7531H2.53147C2.12222 15.7531 1.79722 15.4281 1.79722 15.0188V12.2906C1.79722 11.9531 1.51885 11.6719 1.18135 11.6719C0.843847 11.6719 0.534472 11.9531 0.534472 12.2906V15.0188C0.534472 16.0656 1.48472 16.875 2.53147 16.875H15.4942C16.541 16.875 17.4912 16.0656 17.4912 15.0188V12.2906C17.5222 11.9531 17.2129 11.6719 16.8754 11.6719Z"
+                            fill=""
+                          />
+                          <path
+                            d="M8.55074 12.3469C8.66224 12.4594 8.83199 12.5156 9.00174 12.5156C9.17149 12.5156 9.34124 12.4594 9.45274 12.3469L13.672 8.12746C13.9412 7.85834 13.9412 7.41559 13.672 7.14646C13.4029 6.87734 12.9601 6.87734 12.691 7.14646L9.00174 10.8358L5.28199 7.14646C5.01287 6.87734 4.57012 6.87734 4.30099 7.14646C4.03187 7.41559 4.03187 7.85834 4.30099 8.12746L8.55074 12.3469Z"
+                            fill=""
+                          />
+                          <path
+                            d="M9.00015 12.5156C8.66265 12.5156 8.35327 12.2344 8.35327 11.8969V1.40625C8.35327 1.06875 8.66265 0.7875 9.00015 0.7875C9.33765 0.7875 9.64702 1.06875 9.64702 1.40625V11.8969C9.64702 12.2344 9.33765 12.5156 9.00015 12.5156Z"
+                            fill=""
+                          />
+                        </svg>
+                        Resultados
+                        <svg
+                          className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
+                            open && 'rotate-180'
+                          }`}
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                            fill=""
+                          />
+                        </svg>
+                      </NavLink>
+                      {/* <!-- Dropdown Menu Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && 'hidden'
+                        }`}
+                      >
+                        <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <NavLink
+                              to="/results/categories"
+                              className={({ isActive }) =>
+                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                (isActive && '!text-white')
+                              }
+                            >
+                              <svg
+                                className="fill-current"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M6.10322 0.956299H2.53135C1.5751 0.956299 0.787598 1.7438 0.787598 2.70005V6.27192C0.787598 7.22817 1.5751 8.01567 2.53135 8.01567H6.10322C7.05947 8.01567 7.84697 7.22817 7.84697 6.27192V2.72817C7.8751 1.7438 7.0876 0.956299 6.10322 0.956299ZM6.60947 6.30005C6.60947 6.5813 6.38447 6.8063 6.10322 6.8063H2.53135C2.2501 6.8063 2.0251 6.5813 2.0251 6.30005V2.72817C2.0251 2.44692 2.2501 2.22192 2.53135 2.22192H6.10322C6.38447 2.22192 6.60947 2.44692 6.60947 2.72817V6.30005Z"
+                                  fill=""
+                                />
+                                <path
+                                  d="M15.4689 0.956299H11.8971C10.9408 0.956299 10.1533 1.7438 10.1533 2.70005V6.27192C10.1533 7.22817 10.9408 8.01567 11.8971 8.01567H15.4689C16.4252 8.01567 17.2127 7.22817 17.2127 6.27192V2.72817C17.2127 1.7438 16.4252 0.956299 15.4689 0.956299ZM15.9752 6.30005C15.9752 6.5813 15.7502 6.8063 15.4689 6.8063H11.8971C11.6158 6.8063 11.3908 6.5813 11.3908 6.30005V2.72817C11.3908 2.44692 11.6158 2.22192 11.8971 2.22192H15.4689C15.7502 2.22192 15.9752 2.44692 15.9752 2.72817V6.30005Z"
+                                  fill=""
+                                />
+                              </svg>
+                              Categoria 1
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              to="/results/rounds"
+                              className={({ isActive }) =>
+                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                (isActive && '!text-white')
+                              }
+                            >
+                              <svg
+                                className="fill-current"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M9.0002 2.08105C5.05332 2.08105 1.82832 5.30605 1.82832 9.25293C1.82832 13.1998 5.05332 16.4248 9.0002 16.4248C12.9471 16.4248 16.1721 13.1998 16.1721 9.25293C16.1721 5.30605 12.9471 2.08105 9.0002 2.08105ZM9.0002 15.1748C5.6627 15.1748 3.07832 12.5904 3.07832 9.25293C3.07832 5.91543 5.6627 3.33105 9.0002 3.33105C12.3377 3.33105 14.9221 5.91543 14.9221 9.25293C14.9221 12.5904 12.3377 15.1748 9.0002 15.1748Z"
+                                  fill=""
+                                />
+                                <path
+                                  d="M9.0002 5.70605C8.6627 5.70605 8.35332 5.9873 8.35332 6.3248V9.25293C8.35332 9.5218 8.50957 9.76543 8.75645 9.8718L11.4221 10.9968C11.7158 11.1281 12.0564 10.9968 12.1877 10.7031C12.319 10.4094 12.1877 10.0687 11.894 9.93755L9.64707 8.9343V6.3248C9.64707 5.9873 9.3377 5.70605 9.0002 5.70605Z"
+                                  fill=""
+                                />
+                              </svg>
+                              Categoria 2
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <!-- Dropdown Menu End --> */}
+                    </React.Fragment>
+                  );
+                }}
+              </SidebarLinkGroup>
+            </ul>
+          </div>
+
+          {/* <!-- Botón de Regresar --> */}
+          <div className="mt-8 px-4">
+            <button
+              onClick={handleGoBack}
+              className="flex w-full items-center justify-center gap-3.5 rounded-lg bg-graydark py-3 px-6 text-sm font-medium text-white hover:bg-opacity-90"
+            >
+              <svg
+                className="fill-current"
+                width="16"
+                height="14"
+                viewBox="0 0 16 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.7492 6.38125H2.73984L7.52109 1.51562C7.77422 1.2625 7.77422 0.86875 7.52109 0.615625C7.26797 0.3625 6.87422 0.3625 6.62109 0.615625L0.799219 6.52187C0.546094 6.775 0.546094 7.16875 0.799219 7.42188L6.62109 13.3281C6.73359 13.4406 6.90234 13.525 7.07109 13.525C7.23984 13.525 7.38047 13.4687 7.52109 13.3562C7.77422 13.1031 7.77422 12.7094 7.52109 12.4563L2.76797 7.64687H14.7492C15.0867 7.64687 15.368 7.36562 15.368 7.02812C15.368 6.6625 15.0867 6.38125 14.7492 6.38125Z"
+                  fill=""
+                />
+              </svg>
+              Regresar
+            </button>
+          </div>
+        </nav>
+        {/* <!-- Sidebar Menu --> */}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
