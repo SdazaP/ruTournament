2be01d9b98ce96f2a2782fcaa95dc6ea2e2c7a56
SdazaP <creepersdp57@gmail.com>
2025-06-17 16:46:48 -0600

    refactor(sidebar): update icons and routing paths

--- src/components/Sidebar/Tournament/index.tsx
+++ src/components/Sidebar/Tournament/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import SidebarLinkGroup from './SidebarLinkGroup';
 import Logo from '../../../images/logo/logo.png';
-import ProfileImage from '../../../images/user/user-01.png'; // Asegúrate de tener esta imagen
 
 interface SidebarProps {
   sidebarOpen: boolean;
@@ -181,7 +180,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 <NavLink
                   to="categories"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes('participants') &&
+                    pathname.includes('categories') &&
                     'bg-graydark dark:bg-meta-4'
                   }`}
                 >
@@ -193,16 +192,24 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <path
-                      d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
-                      fill=""
-                    />
-                    <path
-                      d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
-                      fill=""
-                    />
+                    <path d="M6 3H3V6H6V3Z" fill="#FF0000" />{' '}
+                    <path d="M9 3H6V6H9V3Z" fill="#FFFFFF" />
+                    <path d="M12 3H9V6H12V3Z" fill="#0000FF" />
+                    <path d="M6 6H3V9H6V6Z" fill="#FFFFFF" />
+                    <path d="M9 6H6V9H9V6Z" fill="#0000FF" />
+                    <path d="M12 6H9V9H12V6Z" fill="#FFD700" />
+                    <path d="M6 9H3V12H6V9Z" fill="#0000FF" />
+                    <path d="M9 9H6V12H9V9Z" fill="#FFD700" />
+                    <path d="M12 9H9V12H12V9Z" fill="#FF0000" />
+                    <path d="M6 3V12" stroke="#000000" stroke-width="0.5" />
+                    <path d="M9 3V12" stroke="#000000" stroke-width="0.5" />
+                    <path d="M3 6H12" stroke="#000000" stroke-width="0.5" />
+                    <path d="M3 9H12" stroke="#000000" stroke-width="0.5" />
+                    <path d="M12 3L15 5L15 8L12 6V3Z" fill="#990000" />{' '}
+                    <path d="M3 12L6 12L9 15L6 15L3 12Z" fill="#CCCCCC" />{' '}
+                    
                   </svg>
-                  Categorias y Horarios
+                  Categorías y Horarios
                 </NavLink>
               </li>
 
@@ -224,6 +231,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                             : setSidebarExpanded(true);
                         }}
                       >
+                        {/* Icono principal actualizado - Tabla de posiciones */}
                         <svg
                           className="fill-current"
                           width="18"
@@ -233,19 +241,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            d="M16.8754 11.6719C16.5379 11.6719 16.2285 11.9531 16.2285 12.2906V15.0188C16.2285 15.4281 15.9035 15.7531 15.4942 15.7531H2.53147C2.12222 15.7531 1.79722 15.4281 1.79722 15.0188V12.2906C1.79722 11.9531 1.51885 11.6719 1.18135 11.6719C0.843847 11.6719 0.534472 11.9531 0.534472 12.2906V15.0188C0.534472 16.0656 1.48472 16.875 2.53147 16.875H15.4942C16.541 16.875 17.4912 16.0656 17.4912 15.0188V12.2906C17.5222 11.9531 17.2129 11.6719 16.8754 11.6719Z"
-                            fill=""
-                          />
-                          <path
-                            d="M8.55074 12.3469C8.66224 12.4594 8.83199 12.5156 9.00174 12.5156C9.17149 12.5156 9.34124 12.4594 9.45274 12.3469L13.672 8.12746C13.9412 7.85834 13.9412 7.41559 13.672 7.14646C13.4029 6.87734 12.9601 6.87734 12.691 7.14646L9.00174 10.8358L5.28199 7.14646C5.01287 6.87734 4.57012 6.87734 4.30099 7.14646C4.03187 7.41559 4.03187 7.85834 4.30099 8.12746L8.55074 12.3469Z"
-                            fill=""
-                          />
-                          <path
-                            d="M9.00015 12.5156C8.66265 12.5156 8.35327 12.2344 8.35327 11.8969V1.40625C8.35327 1.06875 8.66265 0.7875 9.00015 0.7875C9.33765 0.7875 9.64702 1.06875 9.64702 1.40625V11.8969C9.64702 12.2344 9.33765 12.5156 9.00015 12.5156Z"
+                            d="M3 3H15C15.825 3 16.5 3.675 16.5 4.5V13.5C16.5 14.325 15.825 15 15 15H3C2.175 15 1.5 14.325 1.5 13.5V4.5C1.5 3.675 2.175 3 3 3ZM3 4.5V6H15V4.5H3ZM3 7.5V9H9V7.5H3ZM10.5 7.5V9H15V7.5H10.5ZM3 10.5V12H6V10.5H3ZM7.5 10.5V12H15V10.5H7.5Z"
                             fill=""
                           />
                         </svg>
-                        Resultados
+                        Gestionar Resultados
                         <svg
                           className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
                             open && 'rotate-180'
@@ -273,12 +273,13 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                         <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
                           <li>
                             <NavLink
-                              to="results/categorie1"
+                              to="results/WCA"
                               className={({ isActive }) =>
                                 'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
                                 (isActive && '!text-white')
                               }
                             >
+                              {/* Icono WCA - Cubo Rubik */}
                               <svg
                                 className="fill-current"
                                 width="18"
@@ -287,26 +288,37 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
                               >
+                                <path d="M5.25 3H3V5.25H5.25V3Z" fill="" />
+                                <path d="M9 3H6.75V5.25H9V3Z" fill="" />
+                                <path d="M12.75 3H10.5V5.25H12.75V3Z" fill="" />
+                                <path d="M5.25 6.75H3V9H5.25V6.75Z" fill="" />
+                                <path d="M9 6.75H6.75V9H9V6.75Z" fill="" />
                                 <path
-                                  d="M6.10322 0.956299H2.53135C1.5751 0.956299 0.787598 1.7438 0.787598 2.70005V6.27192C0.787598 7.22817 1.5751 8.01567 2.53135 8.01567H6.10322C7.05947 8.01567 7.84697 7.22817 7.84697 6.27192V2.72817C7.8751 1.7438 7.0876 0.956299 6.10322 0.956299ZM6.60947 6.30005C6.60947 6.5813 6.38447 6.8063 6.10322 6.8063H2.53135C2.2501 6.8063 2.0251 6.5813 2.0251 6.30005V2.72817C2.0251 2.44692 2.2501 2.22192 2.53135 2.22192H6.10322C6.38447 2.22192 6.60947 2.44692 6.60947 2.72817V6.30005Z"
+                                  d="M12.75 6.75H10.5V9H12.75V6.75Z"
                                   fill=""
                                 />
                                 <path
-                                  d="M15.4689 0.956299H11.8971C10.9408 0.956299 10.1533 1.7438 10.1533 2.70005V6.27192C10.1533 7.22817 10.9408 8.01567 11.8971 8.01567H15.4689C16.4252 8.01567 17.2127 7.22817 17.2127 6.27192V2.72817C17.2127 1.7438 16.4252 0.956299 15.4689 0.956299ZM15.9752 6.30005C15.9752 6.5813 15.7502 6.8063 15.4689 6.8063H11.8971C11.6158 6.8063 11.3908 6.5813 11.3908 6.30005V2.72817C11.3908 2.44692 11.6158 2.22192 11.8971 2.22192H15.4689C15.7502 2.22192 15.9752 2.44692 15.9752 2.72817V6.30005Z"
+                                  d="M5.25 10.5H3V12.75H5.25V10.5Z"
+                                  fill=""
+                                />
+                                <path d="M9 10.5H6.75V12.75H9V10.5Z" fill="" />
+                                <path
+                                  d="M12.75 10.5H10.5V12.75H12.75V10.5Z"
                                   fill=""
                                 />
                               </svg>
-                              Categoria 1
+                              Formato WCA
                             </NavLink>
                           </li>
                           <li>
                             <NavLink
-                              to="results/categorie1"
+                              to="results/RB"
                               className={({ isActive }) =>
                                 'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
                                 (isActive && '!text-white')
                               }
                             >
+                              {/* Icono RB - Copa/Trofeo */}
                               <svg
                                 className="fill-current"
                                 width="18"
@@ -316,15 +328,135 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 xmlns="http://www.w3.org/2000/svg"
                               >
                                 <path
-                                  d="M9.0002 2.08105C5.05332 2.08105 1.82832 5.30605 1.82832 9.25293C1.82832 13.1998 5.05332 16.4248 9.0002 16.4248C12.9471 16.4248 16.1721 13.1998 16.1721 9.25293C16.1721 5.30605 12.9471 2.08105 9.0002 2.08105ZM9.0002 15.1748C5.6627 15.1748 3.07832 12.5904 3.07832 9.25293C3.07832 5.91543 5.6627 3.33105 9.0002 3.33105C12.3377 3.33105 14.9221 5.91543 14.9221 9.25293C14.9221 12.5904 12.3377 15.1748 9.0002 15.1748Z"
+                                  d="M9 12.75C12.7279 12.75 15.75 9.72792 15.75 6H13.5C13.5 8.48528 11.4853 10.5 9 10.5C6.51472 10.5 4.5 8.48528 4.5 6H2.25C2.25 9.72792 5.27208 12.75 9 12.75Z"
                                   fill=""
                                 />
                                 <path
-                                  d="M9.0002 5.70605C8.6627 5.70605 8.35332 5.9873 8.35332 6.3248V9.25293C8.35332 9.5218 8.50957 9.76543 8.75645 9.8718L11.4221 10.9968C11.7158 11.1281 12.0564 10.9968 12.1877 10.7031C12.319 10.4094 12.1877 10.0687 11.894 9.93755L9.64707 8.9343V6.3248C9.64707 5.9873 9.3377 5.70605 9.0002 5.70605Z"
+                                  d="M5.25 13.5V15.75H12.75V13.5H5.25Z"
                                   fill=""
                                 />
+                                <path d="M6.75 3V6H11.25V3H6.75Z" fill="" />
                               </svg>
-                              Categoria 2
+                              Formato RB
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <!-- Dropdown Menu End --> */}
+                    </React.Fragment>
+                  );
+                }}
+              </SidebarLinkGroup>
+              <SidebarLinkGroup activeCondition={pathname.includes('view')}>
+                {(handleClick, open) => {
+                  return (
+                    <React.Fragment>
+                      <NavLink
+                        to="#"
+                        className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                          pathname.includes('view') &&
+                          'bg-graydark dark:bg-meta-4'
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded
+                            ? handleClick()
+                            : setSidebarExpanded(true);
+                        }}
+                      >
+                        {/* Icono principal actualizado - Ojo/visualización */}
+                        <svg
+                          className="fill-current"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M9 3.75C5.25 3.75 2.0475 6.3225 1.2 9.75C2.0475 13.1775 5.25 15.75 9 15.75C12.75 15.75 15.9525 13.1775 16.8 9.75C15.9525 6.3225 12.75 3.75 9 3.75ZM9 13.5C6.93 13.5 5.25 11.82 5.25 9.75C5.25 7.68 6.93 6 9 6C11.07 6 12.75 7.68 12.75 9.75C12.75 11.82 11.07 13.5 9 13.5ZM9 7.5C7.755 7.5 6.75 8.505 6.75 9.75C6.75 10.995 7.755 12 9 12C10.245 12 11.25 10.995 11.25 9.75C11.25 8.505 10.245 7.5 9 7.5Z"
+                            fill=""
+                          />
+                        </svg>
+                        Ver Resultados
+                        <svg
+                          className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
+                            open && 'rotate-180'
+                          }`}
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                            fill=""
+                          />
+                        </svg>
+                      </NavLink>
+                      {/* <!-- Dropdown Menu Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && 'hidden'
+                        }`}
+                      >
+                        <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <NavLink
+                              to="view/resultsWCA"
+                              className={({ isActive }) =>
+                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                (isActive && '!text-white')
+                              }
+                            >
+                              {/* Icono WCA - Lista con cubo */}
+                              <svg
+                                className="fill-current"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M3 3H15V5.25H3V3ZM3 6.75H15V9H3V6.75ZM3 10.5H9V12.75H3V10.5Z"
+                                  fill=""
+                                />
+                                <path d="M12 10.5H15V12.75H12V10.5Z" fill="" />
+                                <path d="M10.5 13.5H12V15H10.5V13.5Z" fill="" />
+                              </svg>
+                              Resultados WCA
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              to="view/resultsRB"
+                              className={({ isActive }) =>
+                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                (isActive && '!text-white')
+                              }
+                            >
+                              {/* Icono RB - Podio de competición */}
+                              <svg
+                                className="fill-current"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M4.5 6.75H3V13.5H4.5V6.75Z" fill="" />
+                                <path
+                                  d="M9.75 4.5H8.25V13.5H9.75V4.5Z"
+                                  fill=""
+                                />
+                                <path d="M15 9H13.5V13.5H15V9Z" fill="" />
+                                <path d="M3 13.5H15V15H3V13.5Z" fill="" />
+                              </svg>
+                              Resultados RB
                             </NavLink>
                           </li>
                         </ul>
