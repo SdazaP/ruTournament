9c6bdcb013c3c35a908c37f638352640b684d999
SdazaP <creepersdp57@gmail.com>
2025-06-17 18:05:37 -0600

    refactor(routing): reorganize application routes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ import TournamentWelcome from './pages/Tournament/TournamentWelcome';
 import Participants from './pages/Tournament/Participants';
 import Categories from './pages/Tournament/Categories';
 import ResultsWCA from './pages/Tournament/ResultsWCA';
+import ResultsRB from './pages/Tournament/ResultsRB';
+import ResultsViewRB from './pages/Tournament/Results/ResultsViewRB';
+import ResultsViewWCA from './pages/Tournament/Results/ResultsViewWCA';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -107,7 +110,7 @@ function App() {
                   }
                 />
                 <Route
-                  path='results/categorie1'
+                  path='results/WCA'
                   element={
                     <>
                       <PageTitle title="Competitors Tournament" />
@@ -115,7 +118,35 @@ function App() {
                     </>
                   }
                 />
+                <Route
+                  path='results/RB'
+                  element={
+                    <>
+                      <PageTitle title="Competitors Tournament" />
+                      <ResultsRB />
+                    </>
+                  }
+                />
+                <Route
+                  path='view/resultsRB'
+                  element={
+                    <>
+                      <PageTitle title="Competitors Tournament" />
+                      <ResultsViewRB />
+                    </>
+                  }
+                />
+                <Route
+                  path='view/resultsWCA'
+                  element={
+                    <>
+                      <PageTitle title="Competitors Tournament" />
+                      <ResultsViewWCA />
+                    </>
+                  }
+                />
               </Routes>
+              
             </DashboardTournament>
           </>
         }
