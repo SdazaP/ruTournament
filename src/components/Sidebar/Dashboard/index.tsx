0c64526da572e1cbc8ed38c636ee27b706374baf
SdazaP <creepersdp57@gmail.com>
2026-07-01 15:35:12 -0600

    refactor(sidebar): updated layout

--- src/components/Sidebar/Dashboard/index.tsx
+++ src/components/Sidebar/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import Logo from '../../../images/logo/logo.png';
+import {FiHome} from 'react-icons/fi';
+import { FaBook } from 'react-icons/fa';
 
 interface SidebarProps {
   sidebarOpen: boolean;
@@ -63,7 +65,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
   return (
     <>
-      {/* <!-- Sidebar Backdrop for Mobile --> */}
+      {/* */}
       <div
         onClick={(e) => { e.stopPropagation(); e.preventDefault(); setSidebarOpen(false); }}
         className={`fixed inset-0 z-[999] bg-black/50 lg:hidden ${
@@ -72,52 +74,49 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       ></div>
 
       <aside
-      ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden dark:bg-boxdark bg-white duration-300 ease-linear lg:static lg:translate-x-0 ${
-        sidebarOpen ? 'translate-x-0' : '-translate-x-full'
-      }`}
-    >
-      {/* <!-- SIDEBAR HEADER --> */}
-      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <NavLink to="/dashboard">
-          <img src={Logo} className="invert dark:invert-0" alt="Logo" />
-        </NavLink>
+        ref={sidebar}
+        className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden dark:bg-boxdark bg-white duration-300 ease-linear lg:static lg:translate-x-0 ${
+          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        {/* */}
+        <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5 shrink-0">
+          <NavLink to="/dashboard">
+            <img src={Logo} className="invert dark:invert-0" alt="Logo" />
+          </NavLink>
 
-        <button
-          ref={trigger}
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-          aria-controls="sidebar"
-          aria-expanded={sidebarOpen}
-          className="block lg:hidden"
-        >
-          <svg
-            className="fill-current"
-            width="20"
-            height="18"
-            viewBox="0 0 20 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <button
+            ref={trigger}
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+            aria-controls="sidebar"
+            aria-expanded={sidebarOpen}
+            className="block lg:hidden"
           >
-            <path
-              d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
-              fill=""
-            />
-          </svg>
-        </button>
-      </div>
-      {/* <!-- SIDEBAR HEADER --> */}
+            <svg
+              className="fill-current"
+              width="20"
+              height="18"
+              viewBox="0 0 20 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+                fill=""
+              />
+            </svg>
+          </button>
+        </div>
 
-      <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-        {/* <!-- Sidebar Menu --> */}
-        <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
-          {/* <!-- Menu Group --> */}
-          <div>
+        {/* */}
+        <div className="no-scrollbar flex flex-col flex-1 overflow-y-auto duration-300 ease-linear">
+          <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
               MENU
             </h3>
 
             <ul className="mb-6 flex flex-col gap-1.5">
-              {/* <!-- Menu Item Tournaments --> */}
+              {/* */}
               <li>
                 <NavLink
                   to="/dashboard"
@@ -125,28 +124,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     pathname === '/dashboard' && 'bg-gray-100 dark:bg-meta-4'
                   }`}
                 >
-                  <svg
-                    className="fill-current"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M9.00002 2.025L2.70002 7.125V15.3H6.30002V10.8H11.7V15.3H15.3V7.125L9.00002 2.025ZM9.00002 4.275L13.5 7.65V13.5H12.6V9H5.40002V13.5H4.50002V7.65L9.00002 4.275Z"
-                      fill=""
-                    />
-                  </svg>
+                  <FiHome size={18} />
                   Inicio
                 </NavLink>
               </li>
+
+              {/* */}
               <li>
                 <NavLink
                   to="/dashboard/tournaments"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium dark:text-bodydark1 text-gray-900 duration-300 ease-in-out dark:hover:bg-graydark hover:bg-gray-100 ${
-                    pathname.includes('tournaments') &&
-                    'bg-gray-100 dark:bg-meta-4'
+                    pathname.includes('tournaments') && 'bg-gray-100 dark:bg-meta-4'
                   }`}
                 >
                   <svg
@@ -158,19 +146,19 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      d="M6.10322 0.956299H2.53135C1.5751 0.956299 0.787598 1.7438 0.787598 2.70005V6.27192C0.787598 7.22817 1.5751 8.01567 2.53135 8.01567H6.10322C7.05947 8.01567 7.84697 7.22817 7.84697 6.27192V2.72817C7.8751 1.7438 7.0876 0.956299 6.10322 0.956299ZM6.60947 6.30005C6.60947 6.5813 6.38447 6.8063 6.10322 6.8063H2.53135C2.2501 6.8063 2.0251 6.5813 2.0251 6.30005V2.72817C2.0251 2.44692 2.2501 2.22192 2.53135 2.22192H6.10322C6.38447 2.22192 6.60947 2.44692 6.60947 2.72817V6.30005Z"
+                      d="M5 2H13V5.5C13 7.70914 11.2091 9.5 9 9.5C6.79086 9.5 5 7.70914 5 5.5V2Z"
                       fill=""
                     />
                     <path
-                      d="M15.4689 0.956299H11.8971C10.9408 0.956299 10.1533 1.7438 10.1533 2.70005V6.27192C10.1533 7.22817 10.9408 8.01567 11.8971 8.01567H15.4689C16.4252 8.01567 17.2127 7.22817 17.2127 6.27192V2.72817C17.2127 1.7438 16.4252 0.956299 15.4689 0.956299ZM15.9752 6.30005C15.9752 6.5813 15.7502 6.8063 15.4689 6.8063H11.8971C11.6158 6.8063 11.3908 6.5813 11.3908 6.30005V2.72817C11.3908 2.44692 11.6158 2.22192 11.8971 2.22192H15.4689C15.7502 2.22192 15.9752 2.44692 15.9752 2.72817V6.30005Z"
+                      d="M8 9.5H10V14H13V16H5V14H8V9.5Z"
                       fill=""
                     />
                     <path
-                      d="M6.10322 9.92822H2.53135C1.5751 9.92822 0.787598 10.7157 0.787598 11.672V15.2438C0.787598 16.2001 1.5751 16.9876 2.53135 16.9876H6.10322C7.05947 16.9876 7.84697 16.2001 7.84697 15.2438V11.7001C7.8751 10.7157 7.0876 9.92822 6.10322 9.92822ZM6.60947 15.272C6.60947 15.5532 6.38447 15.7782 6.10322 15.7782H2.53135C2.2501 15.7782 2.0251 15.5532 2.0251 15.272V11.7001C2.0251 11.4188 2.2501 11.1938 2.53135 11.1938H6.10322C6.38447 11.1938 6.60947 11.4188 6.60947 11.7001V15.272Z"
+                      d="M5 2.5H3.5C2.11929 2.5 1 3.61929 1 5C1 6.38071 2.11929 7.5 3.5 7.5H5V6.5H3.5C2.67157 6.5 2 5.82843 2 5C2 4.17157 2.67157 3.5 3.5 3.5H5V2.5Z"
                       fill=""
                     />
                     <path
-                      d="M15.4689 9.92822H11.8971C10.9408 9.92822 10.1533 10.7157 10.1533 11.672V15.2438C10.1533 16.2001 10.9408 16.9876 11.8971 16.9876H15.4689C16.4252 16.9876 17.2127 16.2001 17.2127 15.2438V11.7001C17.2127 10.7157 16.4252 9.92822 15.4689 9.92822ZM15.9752 15.272C15.9752 15.5532 15.7502 15.7782 15.4689 15.7782H11.8971C11.6158 15.7782 11.3908 15.5532 11.3908 15.272V11.7001C11.3908 11.4188 11.6158 11.1938 11.8971 11.1938H15.4689C15.7502 11.1938 15.9752 11.4188 15.9752 11.7001V15.272Z"
+                      d="M13 2.5H14.5C15.8807 2.5 17 3.61929 17 5C17 6.38071 15.8807 7.5 14.5 7.5H13V6.5H14.5C15.3284 6.5 16 5.82843 16 5C16 4.17157 15.3284 3.5 14.5 3.5H13V2.5Z"
                       fill=""
                     />
                   </svg>
@@ -178,68 +166,73 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 </NavLink>
               </li>
 
-              {/* <!-- Menu Item Nuevo Torneo --> */}
+              {/* */}
               <li>
                 <NavLink
                   to="new-tournament"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium dark:text-bodydark1 text-gray-900 duration-300 ease-in-out dark:hover:bg-graydark hover:bg-gray-100 ${
-                    pathname.includes('new-tournament') &&
-                    'bg-gray-100 dark:bg-meta-4'
+                    pathname.includes('new-tournament') && 'bg-gray-100 dark:bg-meta-4'
                   }`}
                 >
-                  <svg
-                    className="fill-current"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M14.0625 6.5625H11.4375V3.9375C11.4375 3.8375 11.3575 3.75 11.25 3.75H9.75C9.6425 3.75 9.5625 3.8375 9.5625 3.9375V6.5625H6.9375C6.83 6.5625 6.75 6.65 6.75 6.75V8.25C6.75 8.35 6.83 8.4375 6.9375 8.4375H9.5625V11.0625C9.5625 11.1625 9.6425 11.25 9.75 11.25H11.25C11.3575 11.25 11.4375 11.1625 11.4375 11.0625V8.4375H14.0625C14.17 8.4375 14.25 8.35 14.25 8.25V6.75C14.25 6.65 14.17 6.5625 14.0625 6.5625Z"
-                      fill=""
-                    />
-                    <path
-                      d="M15.75 2.25H2.25C1.0125 2.25 0 3.2625 0 4.5V13.5C0 14.7375 1.0125 15.75 2.25 15.75H15.75C16.9875 15.75 18 14.7375 18 13.5V4.5C18 3.2625 16.9875 2.25 15.75 2.25ZM16.5 13.5C16.5 13.9125 16.1625 14.25 15.75 14.25H2.25C1.8375 14.25 1.5 13.9125 1.5 13.5V4.5C1.5 4.0875 1.8375 3.75 2.25 3.75H15.75C16.1625 3.75 16.5 4.0875 16.5 4.5V13.5Z"
-                      fill=""
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="currentColor" stroke="currentColor" strokeWidth="0.7" strokeLinejoin="round">
+                    <rect x="1" y="1" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="6.8" y="1" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="12.6" y="1" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="1" y="6.8" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="6.8" y="6.8" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="12.6" y="6.8" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="1" y="12.6" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="6.8" y="12.6" width="4.4" height="4.4" rx="0.5"/>
+                    <rect x="12.6" y="12.6" width="4.4" height="4.4" rx="0.5"/>
                   </svg>
+
                   Nuevo Torneo
                 </NavLink>
               </li>
-            </ul>
-          </div>
 
-          {/* <!-- Botón de Salir --> */}
-          <div className="mt-auto px-4">
-            <button
-              onClick={handleLogout}
-              className="flex w-full items-center justify-center gap-3.5 rounded-lg bg-graydark py-3 px-6 text-sm font-medium text-white hover:bg-opacity-90"
+              {/* */}
+              <li>
+                <NavLink
+                  to="guide"
+                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium dark:text-bodydark1 text-gray-900 duration-300 ease-in-out dark:hover:bg-graydark hover:bg-gray-100 ${
+                    pathname.includes('guide') && 'bg-gray-100 dark:bg-meta-4'
+                  }`}
+                >
+                  <FaBook />
+                  Guía de Usuario
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* */}
+        <div className="shrink-0 p-4 lg:p-6 pb-6">
+          <button
+            onClick={handleLogout}
+            className="flex w-full items-center justify-center gap-3.5 rounded-lg bg-graydark py-3 px-6 text-sm font-medium text-white hover:bg-opacity-90"
+          >
+            <svg
+              className="fill-current"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                className="fill-current"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12.9 2H9.5C9.22386 2 9 2.22386 9 2.5C9 2.77614 9.22386 3 9.5 3H12.9C13.2315 3 13.5 3.26851 13.5 3.6V12.4C13.5 12.7315 13.2315 13 12.9 13H9.5C9.22386 13 9 13.2239 9 13.5C9 13.7761 9.22386 14 9.5 14H12.9C13.7835 14 14.5 13.2835 14.5 12.4V3.6C14.5 2.71651 13.7835 2 12.9 2Z"
-                  fill=""
-                />
-                <path
-                  d="M6.85355 5.14645C6.65829 4.95118 6.34171 4.95118 6.14645 5.14645C5.95118 5.34171 5.95118 5.65829 6.14645 5.85355L8.29289 8H2.5C2.22386 8 2 8.22386 2 8.5C2 8.77614 2.22386 9 2.5 9H8.29289L6.14645 11.1464C5.95118 11.3417 5.95118 11.6583 6.14645 11.8536C6.34171 12.0488 6.65829 12.0488 6.85355 11.8536L9.85355 8.85355C10.0488 8.65829 10.0488 8.34171 9.85355 8.14645L6.85355 5.14645Z"
-                  fill=""
-                />
-              </svg>
-              Salir
-            </button>
-          </div>
-        </nav>
-        {/* <!-- Sidebar Menu --> */}
-      </div>
-    </aside>
+              <path
+                d="M12.9 2H9.5C9.22386 2 9 2.22386 9 2.5C9 2.77614 9.22386 3 9.5 3H12.9C13.2315 3 13.5 3.26851 13.5 3.6V12.4C13.5 12.7315 13.2315 13 12.9 13H9.5C9.22386 13 9 13.2239 9 13.5C9 13.7761 9.22386 14 9.5 14H12.9C13.7835 14 14.5 13.2835 14.5 12.4V3.6C14.5 2.71651 13.7835 2 12.9 2Z"
+                fill=""
+              />
+              <path
+                d="M6.85355 5.14645C6.65829 4.95118 6.34171 4.95118 6.14645 5.14645C5.95118 5.34171 5.95118 5.65829 6.14645 5.85355L8.29289 8H2.5C2.22386 8 2 8.22386 2 8.5C2 8.77614 2.22386 9 2.5 9H8.29289L6.14645 11.1464C5.95118 11.3417 5.95118 11.6583 6.14645 11.8536C6.34171 12.0488 6.65829 12.0488 6.85355 11.8536L9.85355 8.85355C10.0488 8.65829 10.0488 8.34171 9.85355 8.14645L6.85355 5.14645Z"
+                fill=""
+              />
+            </svg>
+            Salir
+          </button>
+        </div>
+      </aside>
     </>
   );
 };
