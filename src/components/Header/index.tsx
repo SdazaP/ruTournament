728f2f476cfc574ca3a050d5c49c33f2caec8e93
SdazaP <creepersdp57@gmail.com>
2026-07-01 18:23:51 -0600

    refactor(header): user area disabled

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -61,7 +61,7 @@ const Header = (props: {
           </Link>
         </div>
 
-        <div className="hidden sm:block">
+        <div className="hidden">
           <form action="https://formbold.com/s/unique_form_id" method="POST">
             <div className="relative">
               <button className="absolute left-0 top-1/2 -translate-y-1/2">
@@ -97,7 +97,7 @@ const Header = (props: {
           </form>
         </div>
 
-        <div className="flex items-center gap-3 2xsm:gap-7">
+        <div className="flex items-center gap-3 2xsm:gap-7 ml-auto">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             {/* <!-- Dark Mode Toggler --> */}
             <DarkModeSwitcher />
@@ -113,7 +113,9 @@ const Header = (props: {
           </ul>
 
           {/* <!-- User Area --> */}
-          <DropdownUser />
+          <div className="hidden">
+            <DropdownUser />
+          </div>
           {/* <!-- User Area --> */}
         </div>
       </div>
